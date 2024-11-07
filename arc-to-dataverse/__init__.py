7254be5d0f48441f99629c03beab22ae08c0e6af
Kevin Schneider <schneider.kev@outlook.de>
2024-11-07 01:06:33 +0100

    small refactoring

--- arc-to-dataverse/__init__.py
+++ arc-to-dataverse/__init__.py
@@ -2,40 +2,40 @@ from arctrl.arc import ARC
 from arctrl.Core.arc_types import Person, ArcInvestigation
 from jsonschema import validate
 import json
-import inspect
 from enum import Enum
 from arctrl.arc import ARC
-#yes.
-from arctrl.JsonIO.investigation import ARCtrl_ArcInvestigation__ArcInvestigation_fromROCrateJsonString_Static_Z721C83C5 as read_inv_from_isa_rocate_string
 from arctrl.Core.arc_types import Person, ArcInvestigation
 from jsonschema import validate
 
+# transpiled function without top level wrapper, needs adjustments in arctrl
+from arctrl.JsonIO.investigation import ARCtrl_ArcInvestigation__ArcInvestigation_fromROCrateJsonString_Static_Z721C83C5 as read_inv_from_isa_rocate_string
+
 def load_json_from_file(file_path):
     """Load JSON data from a file."""
-    with open(file_path, 'r', 'utf-8') as f:
+    with open(file_path, mode='r', encoding='utf-8') as f:
         return json.load(f)
     
 def load_arc_from_rocrate_file (file_path):
-    """Load ARC from a RO-Crate file."""
-    with open(file_path, 'r', 'utf-8') as f:
+    """Load ARC from a RO-Crate file conforming to the ARC ro-crate profile."""
+    with open(file_path, mode='r', encoding='utf-8') as f:
         return ARC.from_rocrate_json_string(f.read())
 
 def load_inv_from_isa_rocrate_file (file_path):
-    """Load Investigation from a ISA RO-Crate file."""
-    with open(file_path, 'r', 'utf-8') as f:
+    """Load Investigation from a RO-Crate file conforming to the ISA ro-crate profile."""
+    with open(file_path, mode='r', encoding='utf-8') as f:
         return read_inv_from_isa_rocate_string(f.read())
 
 
 fairagro_minimal_metadata_block_schema = load_json_from_file(r'C:\Users\schne\source\repos\kMutagene\arc-to-dataverse\arc-to-dataverse\fairagro_minimal_metadata_block_schema_v0.3.json')
 
-# # If no exception is raised by validate(), the instance is valid.
-# validate(instance={"name" : "Eggs", "price" : 34.99}, schema=schema)
-
+# small test file loaded from an isa ro crate
 isa = load_inv_from_isa_rocrate_file(r'C:\Users\schne\source\repos\kMutagene\arc-to-dataverse\arc-to-dataverse\isa-ro-crate-metadata.json')
+# loaded from an arc ro crate
 arc = load_arc_from_rocrate_file(r'C:\Users\schne\source\repos\kMutagene\arc-to-dataverse\arc-to-dataverse\arc-ro-crate-metadata.json')
 
-print(arc.ISA)
-print(isa)
+
+# print(arc.ISA)
+# print(isa)
 
 def map_person_to_author(person:Person):
     return {
@@ -94,5 +94,5 @@ out["citation"]["subject"] = get_subjects(arc.ISA)
 
 validate(instance=out, schema=fairagro_minimal_metadata_block_schema)
 
-with open('test_dataverse_arc_csh.json', 'w') as fp:
+with open('test_dataverse_arc_csh.json', mode='w', encoding='utf-8') as fp:
     json.dump(out, fp)
