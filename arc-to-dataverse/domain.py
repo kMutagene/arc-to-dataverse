e0b2cab97202f34fa3a7aaa4fd05209974b4e4c3
Kevin Schneider <schneider.kev@outlook.de>
2024-11-07 17:26:09 +0100

    update metadata block schema

--- arc-to-dataverse/domain.py
+++ arc-to-dataverse/domain.py
@@ -26,3 +26,25 @@ def load_std_from_isa_rocrate_file (file_path):
     with open(file_path, mode='r', encoding='utf-8') as f:
         return JsonController.Study().from_rocrate_json_string(f.read())
 
+def first(iterable, condition = lambda x: True):
+    """
+    Returns the first item in the `iterable` that
+    satisfies the `condition`.
+
+    If the condition is not given, returns the first item of
+    the iterable.
+
+    Raises `StopIteration` if no item satysfing the condition is found.
+
+    >>> first( (1,2,3), condition=lambda x: x % 2 == 0)
+    2
+    >>> first(range(3, 100))
+    3
+    >>> first( () )
+    Traceback (most recent call last):
+    ...
+    StopIteration
+    """
+
+    return next(x for x in iterable if condition(x))
+
